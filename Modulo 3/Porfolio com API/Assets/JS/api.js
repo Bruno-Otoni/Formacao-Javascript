85ab42b4a27be25c5addff451363be08b8f26482
Bruno-Otoni <botoni@gmail.com>
2024-08-12 01:40:13 -0300

    modificação das informações no profile.json

--- Modulo 3/Porfolio com API/Assets/JS/api.js
+++ Modulo 3/Porfolio com API/Assets/JS/api.js
@@ -1,5 +1,6 @@
 async function fetchProfileData() {
-  const url = 
+  const url =
+    "https://raw.githubusercontent.com/Bruno-Otoni/Formacao-Javascript/main/Modulo%203/Porfolio%20com%20API/Data/profile.json";
   const fetching = await fetch(url);
   return await fetching.json();
 }
